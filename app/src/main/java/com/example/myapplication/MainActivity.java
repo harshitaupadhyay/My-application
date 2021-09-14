9a4e8ac249f80c8e7246dee6a4d53abdf33b1869
harshitaupadhyay <harshitaupa207@gmail.com>
2021-09-14 00:25:38 -0700

    Hello world

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editTextTextPersonName2);
+
+    }
+   public void clickListener(View view){
+        String input;
+        input = editText.getText().toString();
+        textView.setText(input);
+
+   }
+
+}
